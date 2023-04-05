8060e2c11a8d99b7bede5c50953bd326468882eb
thainguyen68 <nvt.689@gmail.com>
2023-04-05 22:32:44 +0700

    day4.1

--- Day4/src/BaiTap/Fan/Fan.java
+++ Day4/src/BaiTap/Fan/Fan.java
@@ -1,2 +1,86 @@
-package BaiTap.Fan;public class Fan {
+package BaiTap.Fan;
+
+public class Fan {
+   //khởi tạo
+    int slow = 1;
+    int medium = 2;
+    int fast = 3;
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+
+    //getter + setter
+    public int getSlow() {
+        return slow;
+    }
+    public void setSlow(int slow) {
+        this.slow = slow;
+    }
+
+    public int getMedium() {
+        return medium;
+    }
+
+    public void setMedium(int medium) {
+        this.medium = medium;
+    }
+
+    public int getFast() {
+        return fast;
+    }
+
+    public void setFast(int fast) {
+        this.fast = fast;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // Constructor khởi tạo đối tượng không tham số
+    public Fan() {
+        this.speed = slow;
+        this.on = false;
+        this.radius = 5;
+        this.color = "blue";
+    }
+
+    // toString() trả về chuỗi chứa thông tin của Fan
+    public String display() {
+        return "Fan :" +
+                "speed=" + speed +
+                ", on=" + on +
+                ", radius=" + radius +
+                ", color=" + color;
+    }
 }
--- Day4/src/BaiTap/Fan/Main.java
+++ Day4/src/BaiTap/Fan/Main.java
@@ -1,2 +1,13 @@
-package BaiTap.Fan;public class Main {
+package BaiTap.Fan;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Fan fan = new Fan();
+        fan.setOn(true);
+        fan.setSpeed(fan.fast);
+        fan.setRadius(10.0);
+        System.out.println(fan.display());
+    }
 }
