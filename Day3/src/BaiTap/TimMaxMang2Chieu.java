06aa92c764913793215691429e891df31354efbe
thainguyen68 <nvt.689@gmail.com>
2023-04-05 09:29:10 +0700

    day3.1

--- Day3/src/BaiTap/TimMaxMang2Chieu.java
+++ Day3/src/BaiTap/TimMaxMang2Chieu.java
@@ -6,20 +6,34 @@ import java.util.Scanner;
 public class TimMaxMang2Chieu {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int[][] arr1 = new int[2][2];
 
-        System.out.println("Nhập độ dài mảng: ");
-        int[][]array = new int[3][3];
-        System.out.println("Nhập phần tử của mảng: ");
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                System.out.println("Nhập vào phần tử " + i + " " + j + " của mảng thứ  ");
+                arr1[i][j] = scanner.nextInt();
+            }
+        }
+        // mảng tạo được:
+        for (int i=0; i< arr1.length; i++ ){
+            System.out.println(Arrays.toString(arr1[i]));
+        }
+        // liên kết phương thức tìm max:
+        arr(arr1);
+    }
 
-
-//        int[]arr1 = new int[5];
-//        arr1[0] = scanner.nextInt();
-//        arr1[1] = scanner.nextInt();
-//        arr1[2] = scanner.nextInt();
-//        arr1[3] = scanner.nextInt();
-//        arr1[4] = scanner.nextInt();
-//        System.out.println("Mảng arr1: " + Arrays.toString(arr1));
-
-
+    
+    // kiểm tra Max:
+    public static void arr(int[][] arr1) {
+        int max = arr1[0][0];
+        for (int i = 0; i < arr1.length ; i++) {
+            for (int j = 0; j < arr1[i].length; j++) {
+                if (max < arr1[i][j]){
+                    max = arr1[i][j];
+                }
+            }
+        }
+        System.out.println("Max:" + max);
     }
 }
+
